a070d0ddfd079c761c392c69c9688fb993bd3efa
Chris Howell <chris.kevin.howell@gmail.com>
2016-01-16 16:09:06 +0000

    Initial commit of project files

---
+++ Solar/Solar/Solar.h
@@ -0,0 +1,19 @@
+//
+//  Solar.h
+//  Solar
+//
+//  Created by Chris Howell on 16/01/2016.
+//  Copyright © 2016 Chris Howell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Solar.
+FOUNDATION_EXPORT double SolarVersionNumber;
+
+//! Project version string for Solar.
+FOUNDATION_EXPORT const unsigned char SolarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Solar/PublicHeader.h>
+
+
