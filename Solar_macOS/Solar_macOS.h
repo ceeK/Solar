2dafb192fcfa7107a8d7a6d806faa3f99e3ccdfe
Michael Hulet <michael@hulet.tech>
2018-09-30 16:18:03 -0400

    Updated project settings, added macOS target

---
+++ Solar_macOS/Solar_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Solar_macOS.h
+//  Solar_macOS
+//
+//  Created by Michael Hulet on 9/30/18.
+//  Copyright © 2018 Chris Howell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Solar_macOS.
+FOUNDATION_EXPORT double Solar_macOSVersionNumber;
+
+//! Project version string for Solar_macOS.
+FOUNDATION_EXPORT const unsigned char Solar_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Solar_macOS/PublicHeader.h>
+
+
