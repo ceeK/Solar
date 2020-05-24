15cc585e313b236db8b55698832b4b4a0f65251d
Erik Montes <zomberik@gmail.com>
2020-05-24 14:37:15 -0700

    Argument 'dependencies' must precede argument 'path'

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Solar",
-            path: "Solar",
-            dependencies: []),
+            dependencies: [],
+            path: "Solar"),
         .testTarget(
             name: "Solar iOSTests",
             dependencies: ["Solar"]),
