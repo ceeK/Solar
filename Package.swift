9ab6ccbe5d9ae48070be3eb66bf81c543f0dc803
Erik Montes <zomberik@gmail.com>
2020-05-24 14:30:27 -0700

    .v10_9 is not availble, using .v10_10 instead https://developer.apple.com/documentation/swift_packages/supportedplatform/macosversion

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v8),
         .watchOS(.v3),
         .tvOS(.v9),
-        .macOS(.v10_9),
+        .macOS(.v10_10),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
